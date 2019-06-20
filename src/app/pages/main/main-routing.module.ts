7a08aff8047d68e1615b43dd1aa145a821ae6dd0
ganyun <2211796911@qq.com>
2019-06-20 16:58:54 +0800

    目录 remove

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
-// import { DetailComponent } from './detail/detail.component';
+// import { DetailComponent } from './detail/detail.component'; // 右边 待办详情
 import { InitGuardService } from '../../services/init-guard/init-guard.service';
 
 
@@ -14,7 +14,7 @@ const routes: Routes = [
     // children: [
     //   {
     //     path: ':id',
-    //     component: DetailComponent,
+    //     component: DetailComponent, // 右边 待办详情
     //     pathMatch: 'full'
     //   }
     // ]
